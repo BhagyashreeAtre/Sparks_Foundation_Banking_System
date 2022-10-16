5037c87e60afb62f8ba6e35b9e5e6ef7f0479bf7
BhagyashreeAtre <bhagyashriatre08@gmail.com>
2022-10-16 10:27:23 +0530

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-secondary">
+      <a class="navbar-brand" href="index.php" style="color : #fe3f40;"><b>INDIAN BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color:#2a2a2a;"><b onMouseOver="this.style.color=' #fe3f40'"  onMouseOut="this.style.color='#2a2a2a'">Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #2a2a2a;"><b onMouseOver="this.style.color=' #fe3f40'"  onMouseOut="this.style.color='#2a2a2a'">Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #2a2a2a;"><b onMouseOver="this.style.color=' #fe3f40'"  onMouseOut="this.style.color='#2a2a2a'">Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #2a2a2a;"><b onMouseOver="this.style.color=' #fe3f40'"  onMouseOut="this.style.color='#2a2a2a'">Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
+      
